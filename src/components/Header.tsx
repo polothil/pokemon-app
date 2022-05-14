8c86171ae1fb7b50a2896b680ad47429dcc55e83
aravindpvk <aravindpvk@gmail.com>
2022-05-14 22:46:54 +0200

    css changes 2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,16 @@ const Header: React.FC<HeaderProps> = ({ filter, fav, showFav }) => {
   };
   return (
     <div className='header'>
-      <h1>Pokemon App</h1>
+      <h1>
+        <span>
+          <img
+            src='https://cdn.icon-icons.com/icons2/896/PNG/512/pokemon_go_play_game_cinema_film_movie_icon-icons.com_69163.png'
+            alt=''
+            style={{ height: '40px', marginRight: '20px', marginBottom: '-10px' }}
+          />
+        </span>
+        Pokemon App
+      </h1>
       <div className='filter'>
         <input
           type='text'
